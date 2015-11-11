2dbdb70dad12db6b883edd609bac4a02c351a104
ryu <ryu@development>
2015-11-11 15:30:07 +0100

    Eclipse Push test

---
+++ test/src/newPackage/Test.java
@@ -0,0 +1,10 @@
+package newPackage;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("test test i guess it worked");
+	}
+
+}
